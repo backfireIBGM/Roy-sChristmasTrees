9dcc9bbdef13d6816186c1c2b8a905cf53ce3d95
backfireIBGM <thebeast4028@gmail.com>
2025-02-27 15:13:13 -0800

    first day

---
+++ homePage.js
@@ -0,0 +1,13 @@
+document.body.innerHTML += '<header>Roy\'s Christmas Trees</header>';
+
+document.body.innerHTML += '<img src="Ornaments.jpg" id="ornaments">';
+document.body.innerHTML += '<img src="RoyAtFarm.jpg" id="roy-farm">';
+document.body.innerHTML += '<img src="Tree.jpg" id="tree">';
+document.body.innerHTML += '<img src="Wreath.jpg" id="wreath">';
+document.body.innerHTML += '<img src="WreathWithCandle.jpg" id="wreath-candle">';
+
+document.body.innerHTML += '<h3>Address</h3>';
+document.body.innerHTML += '<p>This is paragraph text</p>';
+
+document.body.innerHTML += '<h3>phone number</h3>';
+document.body.innerHTML += '<p>(707)-###-####)</p>';
