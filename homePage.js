51aafb0696d3c5bb4f94aeafe655661aefcf800d
backfireIBGM <thebeast4028@gmail.com>
2025-03-25 15:42:01 -0700

    phone #

--- homePage.js
+++ homePage.js
@@ -32,7 +32,7 @@ PhoneNumHeading.textContent = '';
 document.body.appendChild(PhoneNumHeading);
 const PhoneNumText = document.createElement('h1');
 PhoneNumText.id = 'address-text';
-PhoneNumText.textContent = '(707) 834-3478';
+PhoneNumText.textContent = '(707) 834-6495';
 document.body.appendChild(PhoneNumText);
 
 // Create main content wrapper
